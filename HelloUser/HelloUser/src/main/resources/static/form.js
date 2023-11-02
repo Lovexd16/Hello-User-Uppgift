7747db5b843bcdc8282eb0ebf6dc20cb00ca6ddf
Love Rumar Karlquist <lovebandy@hotmail.se>
2023-11-02 09:00:57 +0100

    Har gjort så elemtent som inte ska visas tas bort istället för bara döljs

--- HelloUser/HelloUser/src/main/resources/static/form.js
+++ HelloUser/HelloUser/src/main/resources/static/form.js
@@ -2,13 +2,11 @@ let checkAdmin = localStorage.getItem('username'); //Hämta username från local
 let adminAccess = document.getElementById('adminAccess'); //Hämta element med id adminAccess
 let notLoggedInMessage = document.getElementById('notLoggedInMessage'); //Hämta element med id notLoggedInMessage
 
-//Om username är admin visas adminAccess, och notLoggedInMessage döljs
+//Om username är admin visas adminAccess, och notLoggedInMessage tas bort
 if (checkAdmin === 'admin') { 
-    adminAccess.style.display = 'block';
-    notLoggedInMessage.style.display = 'none';
+    notLoggedInMessage.remove();
 
-//Om username inte är admin döljs adminAccess och notLoggedInMessage visas
+//Om username inte är admin tas element med id adminAccess bort
 } else {
-    adminAccess.style.display = 'none';
-    notLoggedInMessage.style.display = 'block';
+    adminAccess.remove();
 }
--- HelloUser/HelloUser/src/main/resources/static/member.js
+++ HelloUser/HelloUser/src/main/resources/static/member.js
@@ -2,12 +2,9 @@ let checkAdmin = localStorage.getItem('username');
 
 let deleteMemberLinks = document.querySelectorAll(".delete-member-link"); //Hämtar alla element med class "delete-member-link" och gör variabel
 
-if (checkAdmin === 'admin') {
-    for (var i = 0; i < deleteMemberLinks.length; i++) { //Ittererar igenom listan som jag gav class "delete-member-link där i används som räknare."
-        deleteMemberLinks[i].style.display = "inline"; //Ändrar style till inline för varje element som hämtades
-    }
-} else {
-    for (var i = 0; i < deleteMemberLinks.length; i++) {
-        deleteMemberLinks[i].style.display = "none"; //Ändrar style till none för varje element som hämtades. Alltså döljs.
+if (checkAdmin != 'admin') {
+    for (var i = 0; i < deleteMemberLinks.length; i++) { //Ittererar igenom listan som jag gav class "delete-member-link". i används som räknare
+        deleteMemberLinks[i].remove(); //Tar bort alla deleteMemberLinks
     }
 }
+
